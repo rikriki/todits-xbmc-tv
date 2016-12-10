45b8b5de1096f4820f032c0dcdf92293aea02c53
rikriki <frederick.rosales@gmail.com>
2016-12-09 23:12:58 -0500

    update tfc.tc

--- plugin.video.tfctv/default.py
+++ plugin.video.tfctv/default.py
@@ -7,7 +7,7 @@ common = CommonFunctions
 thisAddon = xbmcaddon.Addon()
 common.plugin = thisAddon.getAddonInfo('name')
 baseUrl = 'http://tfc.tv'
-
+cookie = ".TFCTV=FC975A5A90151CFA2868D70BFE9F11FF5A00DE0B9736C6984A03C7E28DEEC56532A4978761CEF1AACDE5F278A0B444A3961D3D81B5029C14C65DCDFBEB81BC1FF0C669F31D555134507341151FACCAAC8895AD024E73DC55EEEED662F4B7B4A901E177935120F1E21E58C0781A8A77108297652AB2006C60675EFA78ED12695443DFA564B03A03E253A037A3E781A897D6557A1D31DFC26315F5268ED84A6C5932361AF088D1CBA7A607050F8D687AF7E8546418; __cfduid=da4d1f6d98f59b1fb5227928c9061d76b1481312063; optimizelyEndUserId=oeu1481312063875r0.6144768789276718; __gads=ID=9be238905ef96833:T=1481312064:S=ALNI_MbXONQz3_1koOfFaLgFvLV_mBsW9A; regcook=274fa181-bad2-4099-af5f-fb383a56491a; __RequestVerificationToken=zrCszCF8No0H4ZR7nflRkEsEcQ4EYrNof73LZXyontCvUX5onfnZ8qq3hk90feGll1hRzQOXRsrQtYT4SWTEd_wZic8QSQDJTafccROW1kBcJ_qNWRds391DFH0lmx6CChrTTQ2; rcDate=Fri, 09 Dec 2016 20:35:37 GMT; glt_2_6Y5KLYxUvG2qbK6AoEjHQIISiJvj99Li5cluBktSMTIrFPU2T3DRC8Iz_r0xRiNE=LT3_DjgKcci_q5RhQPypQpTVB_1AgGo67m2ERZcXxNKBYJ0%7CUUID%3Dc81a1a1dffb641749c603b817168a4b8; optmzl=3; optimizelySegments=%7B%221755021167%22%3A%22false%22%2C%221766442549%22%3A%22direct%22%2C%221774641359%22%3A%22gc%22%7D; optimizelyBuckets=%7B%7D; _ceg.s=ohxqxk; _ceg.u=ohxqxk; _dc_gtm_UA-2265816-2=1; _ga=GA1.2.658898550.1481312065; _em_t=true; _em_vt=42afcb0ba1527f919b1733ab7a6a584b07411f1340-25949054584b1799; _em_v=7d29cbe57b2b1de31b12d5926127584b14fe6e6b81-40934826584b1799" 
 # common.dbg = True # Default
 # common.dbglevel = 3 # Default
 
@@ -153,7 +153,7 @@ def playEpisode(episode):
         return xbmcplugin.setResolvedUrl(thisPlugin, True, liz)
     else:
         if (not episodeDetails) or (episodeDetails and episodeDetails.has_key('errorCode') and episodeDetails['errorCode'] != 0):
-            xbmc.executebuiltin('Notification(%s, %s)' % ('Media Error', 'Subscription is already expired or the item is not part of your subscription.'))
+            xbmc.executebuiltin('Notification(%s, %s)' % ('Media Error', 'FOO Subscription is already expired or the item is not part of your subscription.'))
     return False
     
 def get_media_info(episode):
@@ -294,6 +294,8 @@ def showSubcriptionInformation():
 def callServiceApi(path, params = {}, headers = [], base_url = baseUrl):
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJar))
     headers.append(('User-Agent', userAgent))
+    headers.append(('Cookie', cookie))
+
     opener.addheaders = headers
     if params:
         data_encoded = urllib.urlencode(params)
